31b91490bf3629b60968a0e06b679e3f7a60f667
roshan kumar <60356811+roshanKumar811@users.noreply.github.com>
2023-05-01 22:01:06 -0400

    Delete springboot directory

--- springboot/src/main/java/com/example/springboot/StudentRepository/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.springboot.StudentRepository;
-
-import com.example.springboot.model.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<Student, Integer> {
-
-    Student findByName(String name);
-}
